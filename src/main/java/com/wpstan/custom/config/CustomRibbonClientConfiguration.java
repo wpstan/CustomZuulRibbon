5143cd0a9ae17c638c833dce97b81f7fef868777
wpstan <wpstan@github.com>
2020-01-08 22:35:59 +0800

    #commit MaxRetries设置为2

--- src/main/java/com/wpstan/custom/config/CustomRibbonClientConfiguration.java
+++ src/main/java/com/wpstan/custom/config/CustomRibbonClientConfiguration.java
@@ -26,8 +26,13 @@ public class CustomRibbonClientConfiguration extends RibbonClientConfiguration {
         IClientConfig config = super.ribbonClientConfig();
         //可以增加一些配置，例如Ping后台主机的时间间隔5s
         config.set(CommonClientConfigKey.NFLoadBalancerPingInterval, 5);
-        //可以在此设置Ping实现类，也可以使用方法直接构造，例如下面的ribbonPing()方法
+        //可以在此设置Ping实现类（无需重写ribbonPing，base中会根据如下配置反射构造IPing），也可以使用方法直接构造，例如下面的ribbonPing()方法
         config.set(CommonClientConfigKey.NFLoadBalancerPingClassName,"com.wpstan.custom.ribbon.CustomPing");
+        //设置MaxAutoRetries为2，确保某个服务挂的瞬间，ServerStats的Successive Connection Failure大于或等于
+        //ServerStats中的connectionFailureThreshold的默认次数3。这样ServerStats的isCircuitBreakerTripped才被置位短路。
+        //在Retry重试的时候，CustomRule中Choose下一个服务的时候，确保之前的服务已经短路，选用下一个服务，否则可能继续选择不可用服务导致报错。
+        //还有一种方式就是调小ServerStats的connectionFailureThreshold。
+        config.set(CommonClientConfigKey.MaxAutoRetries, 2);
         return config;
     }
 
